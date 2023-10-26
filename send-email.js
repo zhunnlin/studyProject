0a11ac3fa7fa6d3283cb5323b571fe82c365cbe0
zhunnlin <zhunannan01@daojia-inc.com>
2023-10-26 21:02:19 +0800

    feat: 333

--- send-email.js
+++ send-email.js
@@ -1,34 +1,36 @@
-const nodemailer = require("nodemailer");
-const fs = require("fs");
-const path = require("path");
+// const nodemailer = require("nodemailer");
+// const fs = require("fs");
+// const path = require("path");
 
-// 配置您的邮箱信息
-const transporter = nodemailer.createTransport({
-  host: 'smtp.163.com',//process.env.SMTP_SERVER,//代表邮箱的主机：163邮箱是'smtp.163.com' qq是'smtp.qq.com'
-  port: 465,
-  pool: true,
-  secure: true, // 使用 SSL
-  auth: {
-    user: "18224593146@163.com", // 发送邮件的邮箱地址
-    pass: "JBHLJBRWMPJKTROL",// process.env.SMTP_PASSWORD, // 发送邮件的邮箱密码--授权密码
-  },
-});
-// console.log(process.env.SMTP_PASSWORD)
-// 邮件内容
-const mailOptions = {
-  from: "18224593146@163.com", // 发件人邮箱
-  to: "18224593146@163.com",// process.env.SMTP_SEND_TO, // 收件人邮箱
-  subject: "定时邮件测试标题", // 邮件主题
-  // text: "这是一封定时发送的测试邮件。", // 邮件内容
-  html: fs.createReadStream(path.resolve(__dirname, "email.html")),
-  attachments: []//附件
-};
+console.log(process.argv.slice(-1))
 
-// 定时任务
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.error("Error sending email:", error);
-  } else {
-    console.log("Email sent:", info.response);
-  }
-});
+// // 配置您的邮箱信息
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.163.com',//process.env.SMTP_SERVER,//代表邮箱的主机：163邮箱是'smtp.163.com' qq是'smtp.qq.com'
+//   port: 465,
+//   pool: true,
+//   secure: true, // 使用 SSL
+//   auth: {
+//     user: "18224593146@163.com", // 发送邮件的邮箱地址
+//     pass: "JBHLJBRWMPJKTROL",// process.env.SMTP_PASSWORD, // 发送邮件的邮箱密码--授权密码
+//   },
+// });
+// // console.log(process.env.SMTP_PASSWORD)
+// // 邮件内容
+// const mailOptions = {
+//   from: "18224593146@163.com", // 发件人邮箱
+//   to: "18224593146@163.com",// process.env.SMTP_SEND_TO, // 收件人邮箱
+//   subject: "定时邮件测试标题", // 邮件主题
+//   // text: "这是一封定时发送的测试邮件。", // 邮件内容
+//   html: fs.createReadStream(path.resolve(__dirname, "email.html")),
+//   attachments: []//附件
+// };
+
+// // 定时任务
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     console.error("Error sending email:", error);
+//   } else {
+//     console.log("Email sent:", info.response);
+//   }
+// });
