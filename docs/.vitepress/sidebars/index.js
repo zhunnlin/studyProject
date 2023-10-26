0b740d0eea7f740b8da0ab2e707625cc4b68ad98
zhunnlin <zhunannan01@daojia-inc.com>
2023-10-26 20:24:19 +0800

    feat:1

---
+++ docs/.vitepress/sidebars/index.js
@@ -0,0 +1,40 @@
+
+export function getSidebar() {
+  return {
+    "allSidebar": [
+      {
+        "text": "2023-10-09",
+        "collapsible": true,
+        "collapsed": false,
+        "items": [
+          {
+            "text": "github 高星项目",
+            "link": "/statistics/"
+          }
+        ]
+      },
+      {
+        "text": "2023-10-09",
+        "collapsible": true,
+        "collapsed": false,
+        "items": [
+          {
+            "text": "github 高星项目",
+            "link": "/statistics/2023-10-09"
+          }
+        ]
+      },
+      {
+        "text": "2023-10-12",
+        "collapsible": true,
+        "collapsed": false,
+        "items": [
+          {
+            "text": "github 高星项目",
+            "link": "/statistics/2023-10-10"
+          }
+        ]
+      }
+    ]
+  }
+}
