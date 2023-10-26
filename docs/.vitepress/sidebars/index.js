3ea11934f6dcc6a873e729e671af81bf0bbaaee2
zhunnlin <zhunannan01@daojia-inc.com>
2023-10-26 19:56:55 +0800

    feat:test

--- docs/.vitepress/sidebars/index.js
+++ docs/.vitepress/sidebars/index.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const fs = require("fs").promises;
+import path from 'path'
+import fs from 'fs/promises'
 
 const filePath = path.join(__dirname, "./allSidebar.json");
 
