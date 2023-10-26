dc68dbef26c4ec4c04cb46fe5d593df5532d86f0
zhunnlin <zhunannan01@daojia-inc.com>
2023-10-26 19:32:55 +0800

    feat: test

--- send-email.js
+++ send-email.js
@@ -4,20 +4,20 @@ const path = require("path");
 
 // 配置您的邮箱信息
 const transporter = nodemailer.createTransport({
-  host: 'imap.163.com',//process.env.SMTP_SERVER,//代表邮箱的主机：163邮箱是'smtp.163.com' qq是'smtp.qq.com'
+  host: 'smtp.163.com',//process.env.SMTP_SERVER,//代表邮箱的主机：163邮箱是'smtp.163.com' qq是'smtp.qq.com'
   port: 465,
   pool: true,
   secure: true, // 使用 SSL
   auth: {
     user: "18224593146@163.com", // 发送邮件的邮箱地址
-    pass:"JBHLJBRWMPJKTRO",// process.env.SMTP_PASSWORD, // 发送邮件的邮箱密码--授权密码
+    pass: "JBHLJBRWMPJKTROL",// process.env.SMTP_PASSWORD, // 发送邮件的邮箱密码--授权密码
   },
 });
 // console.log(process.env.SMTP_PASSWORD)
 // 邮件内容
 const mailOptions = {
   from: "18224593146@163.com", // 发件人邮箱
-  to:"18224593146@163.com",// process.env.SMTP_SEND_TO, // 收件人邮箱
+  to: "18224593146@163.com",// process.env.SMTP_SEND_TO, // 收件人邮箱
   subject: "定时邮件测试标题", // 邮件主题
   // text: "这是一封定时发送的测试邮件。", // 邮件内容
   html: fs.createReadStream(path.resolve(__dirname, "email.html")),
